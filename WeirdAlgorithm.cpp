9090b32782d424938bd8cc8fb1b5add29ff92df2
yassine4real <yssnabbou2002@gmail.com>
2024-11-09 03:01:42 +0100

    first commit

---
+++ WeirdAlgorithm.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    while(n>1){
+        cout << n << " ";
+        if(n%2){
+            n=n*3+1;
+        }else{
+            n/=2;
+        }
+    }
+    cout<<n<<'\n';
+    return 0;
+}
